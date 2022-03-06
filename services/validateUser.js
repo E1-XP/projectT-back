25d52465fbfc99c75d2334122241dd224fa71e3a
E1-XP <poczta.elektroniczna@hotmail.com>
2022-03-06 16:50:59 +0100

    allow spaces in username validation

--- services/validateUser.js
+++ services/validateUser.js
@@ -1,7 +1,7 @@
 module.exports = function validateUser(obj) {
   const emailRegExp =
     /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  const usernameRegExp = /^([a-zA-Z0-9_-]){2,32}$/;
+  const usernameRegExp = /^([a-zA-Z0-9 _-]){2,32}$/;
   const passwordRegExp = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/;
 
   if (obj["email"] && !new RegExp(emailRegExp).test(obj.email)) return false;
